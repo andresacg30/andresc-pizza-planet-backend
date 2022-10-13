8dbc86c4384e39dbbc208ecb1e96a825bf82cad8
Andres C <andresacg30@gmail.com>
2022-10-13 19:01:24 -0400

    add beverage fixtures

--- app/repositories/models.py
+++ app/repositories/models.py
@@ -37,7 +37,7 @@ class OrderDetail(db.Model):
     ingredient = db.relationship('Ingredient', backref=db.backref('ingredient'))
 
 
-class Beverage(db.model):
+class Beverage(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
---
+++ app/test/fixtures/beverage.py
@@ -0,0 +1,40 @@
+import pytest
+
+from ..utils.functions import get_random_price, get_random_string
+
+
+def beverage_mock() -> dict:
+    return {
+        'name': get_random_string(),
+        'price': get_random_price(10, 20)
+    }
+
+
+@pytest.fixture
+def beverage_uri():
+    return '/beverage/'
+
+
+@pytest.fixture
+def beverage():
+    return beverage_mock()()
+
+
+@pytest.fixture
+def beverages():
+    return [beverage_mock()() for _ in range(5)]
+
+
+@pytest.fixture
+def create_beverage(client, beverage_uri) -> dict:
+    response = client.post(beverage_uri, json=beverage_mock()())
+    return response
+
+
+@pytest.fixture
+def create_beverages(client, beverage_uri) -> list:
+    ingredients = []
+    for _ in range(10):
+        new_ingredient = client.post(beverage_uri, json=beverage_mock()())
+        ingredients.append(new_ingredient.json)
+    return ingredients
