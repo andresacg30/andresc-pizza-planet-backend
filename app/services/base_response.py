b8c480d1e73bbe37d649fe3bc2558f7857133db0
Andres C <andresacg30@gmail.com>
2022-10-13 09:39:01 -0400

    Added base class for refactor services

---
+++ app/services/base_response.py
@@ -0,0 +1,25 @@
+from flask import jsonify
+
+from app.controllers.base import BaseController
+
+
+class BaseResponse:
+    def __init__(self, controller: BaseController):
+        self.controller = controller
+
+    def receive_response(self):
+        self.response = (
+            self.controller_request if not self.error else {"error": self.error}
+        )
+
+    def get_status_code(self):
+        self.status_code = 200 if not self.error else 400
+
+    def jsonify_response(self):
+        return jsonify(self.response), self.status_code
+
+    def get_response(self, request: tuple):
+        self.controller_request, self.error = request
+        self.receive_response()
+        self.get_status_code()
+        return self.jsonify_response()
