769cab1841932df0e86b60746d4f26f03edf8eb2
Andres C <andresacg30@gmail.com>
2022-10-13 12:04:05 -0400

    Create new function for build responses on services

--- app/services/ingredient.py
+++ app/services/ingredient.py
@@ -2,31 +2,31 @@ from app.common.http_methods import GET, POST, PUT
 from flask import Blueprint, request
 
 from ..controllers import IngredientController
-from .base_response import BaseResponse
+from ..common.utils import response_builder
+
 
 ingredient = Blueprint('ingredient', __name__)
-ingredient_response = BaseResponse(IngredientController)
 
 
 @ingredient.route('/', methods=POST)
 def create_ingredient():
-    response = ingredient_response.controller.create(request.json)
-    return ingredient_response.get_jsonify_response(response)
+    response = IngredientController.create(request.json)
+    return response_builder(response)
 
 
 @ingredient.route('/', methods=PUT)
 def update_ingredient():
-    response = ingredient_response.controller.update(request.json)
-    return ingredient_response.get_jsonify_response(response)
+    response = IngredientController.update(request.json)
+    return response_builder(response)
 
 
 @ingredient.route('/id/<_id>', methods=GET)
 def get_ingredient_by_id(_id: int):
-    response = ingredient_response.controller.get_by_id(_id)
-    return ingredient_response.get_jsonify_response(response)
+    response = IngredientController.get_by_id(_id)
+    return response_builder(response)
 
 
 @ingredient.route('/', methods=GET)
 def get_ingredients():
-    response = ingredient_response.controller.get_all()
-    return ingredient_response.get_jsonify_response(response)
+    response = IngredientController.get_all()
+    return response_builder(response)
