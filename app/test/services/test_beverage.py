f32162f8950e7ab14c02ce4adc43cf04e0442244
Andres C <andresacg30@gmail.com>
2022-10-14 12:05:48 -0400

    added test for update beverage: failed

--- app/test/services/test_beverage.py
+++ app/test/services/test_beverage.py
@@ -6,3 +6,13 @@ from app.test.utils.functions import get_random_string, get_random_price
 def test_create_beverage_service(create_beverage):
     beverage = create_beverage.json
     pytest.assume(create_beverage.status.startswith('200'))
+    pytest.assume(beverage['_id'])
+    pytest.assume(beverage['name'])
+    pytest.assume(beverage['price'])
+
+
+def test_update_beverage_service(client, create_beverage, beverage_uri):
+    current_beverage = create_beverage.json
+    update_data = {**current_beverage, 'name': get_random_string(), 'price': get_random_price(2, 7)}
+    response = client.put(beverage_uri, json=update_data)
+    pytest.assume(response.status.startswith('200'))
