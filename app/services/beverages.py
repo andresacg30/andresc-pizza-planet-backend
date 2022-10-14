18cc225a6663090bb63e0cd7bc31209c0f8ab764
Andres C <andresacg30@gmail.com>
2022-10-14 12:21:08 -0400

    added test for get by id beverage service: passed

--- app/services/beverages.py
+++ app/services/beverages.py
@@ -18,3 +18,9 @@ def create_beverage():
 def update_ingredient():
     response = BeverageController.update(request.json)
     return response_builder(response)
+
+
+@beverage.route('/id/<_id>', methods=GET)
+def get_beverage_by_id(_id: int):
+    response = BeverageController.get_by_id(_id)
+    return response_builder(response)
