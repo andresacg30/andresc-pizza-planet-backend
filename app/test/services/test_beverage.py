d0fa2a1a3aee157d844ffa7058513f8114e68ff4
Andres C <andresacg30@gmail.com>
2022-10-13 23:16:32 -0400

    Added test for create beverage service: failed

---
+++ app/test/services/test_beverage.py
@@ -0,0 +1,8 @@
+import pytest
+
+from app.test.utils.functions import get_random_string, get_random_price
+
+
+def test_create_beverage_service(create_beverage):
+    beverage = create_beverage.json
+    pytest.assume(create_beverage.status.startswith('200'))
