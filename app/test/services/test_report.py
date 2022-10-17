d8b69c9d86ab3a9ecc26c67d1ebfdbf749dd6df6
Andres C <andresacg30@gmail.com>
2022-10-17 10:28:16 -0400

    report service test: passed

--- app/test/services/test_report.py
+++ app/test/services/test_report.py
@@ -3,6 +3,9 @@ import pytest
 
 def test_get_report_service(client, report_uri, create_orders):
     response = client.get(report_uri)
-    report = response
+    report = response.json
 
     pytest.assume(response.status.startswith('200'))
+    pytest.assume(report['most_requested_ingredient'])
+    pytest.assume(report['month_with_most_revenue'])
+    pytest.assume(report['top_clients'])
